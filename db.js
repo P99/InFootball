9a02931ea97d5fae6e96008f1236b9efd5362126
Pascal <Palou@imac.default>
2015-05-17 21:28:06 +0100

    Fix mongo db connection URL

--- db.js
+++ db.js
@@ -1,4 +1,4 @@
 module.exports = {
 	//'url' : 'mongodb://<dbuser>:<dbpassword>@novus.modulusmongo.net:27017/<dbName>'
-	'url' : (process.env.OPENSHIFT_MONGODB_DB_URL || 'mongodb://localhost:27017' ) + '/infootball'
+	'url' : (process.env.OPENSHIFT_MONGODB_DB_URL || 'mongodb://localhost:27017/' ) + 'infootball'
 }
