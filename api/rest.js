d6e372465d411bb852de40f9aded5f8c8a344fc7
Pascal Jacquemart <p99_pascal@yahoo.fr>
2015-05-19 00:27:46 +0100

    fix mismatch socket.io package and server

--- api/rest.js
+++ api/rest.js
@@ -5,7 +5,7 @@ module.exports = function(server) {
     socketIO.on('connection', function (socket) {
         console.log('user connected');
         socket.on('chat message', function(msg){
-            io.emit('chat message', msg);
+            socketIO.emit('chat message', msg);
         });
         socket.on('disconnect', function() {
             console.log('user disconnected');
