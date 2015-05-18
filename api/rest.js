a8b5ef3a3dbaf8a9d7868be4bc290a4e2713874f
Pascal <Palou@imac.default>
2015-05-19 00:24:04 +0100

    Adding chat reply to messages

--- api/rest.js
+++ api/rest.js
@@ -4,6 +4,9 @@ module.exports = function(server) {
     var socketIO = io(server);
     socketIO.on('connection', function (socket) {
         console.log('user connected');
+        socket.on('chat message', function(msg){
+            io.emit('chat message', msg);
+        });
         socket.on('disconnect', function() {
             console.log('user disconnected');
         });
