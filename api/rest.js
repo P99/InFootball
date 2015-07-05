b25946e7e170a289ab47c949284840d75db0335e
Pascal Jacquemart <p99_pascal@yahoo.fr>
2015-07-05 16:55:19 +0100

    Adding template update and create actions

--- api/rest.js
+++ api/rest.js
@@ -8,7 +8,7 @@ module.exports = function(server) {
     var socketIO = io(server);
     var templateSocket = socketIO.of('/template');
     templateSocket.on('connection', function (socket) {
-        socket.on('message', function(msg){
+        socket.on('message', function(msg) {
             var _method = msg.method;
             var _uri = msg.uri;
             var _data = msg.data;
@@ -28,6 +28,24 @@ module.exports = function(server) {
                       console.log("Error creating template: " + err);
                     }
                   });
+            } else if ((_method === "UPDATE") && (_uri.indexOf("templates/") == 0)) {
+              console.log("Updating template: " + JSON.stringify(_data));
+              Template.findOneAndUpdate({_id: _data._id}, _data, function(err) {
+                if (!err) {
+                  // Dispatch update
+                  console.log("Updated");
+                  notifyAll();
+                }
+              });
+            } else if ((_method === "DELETE") && (_uri.indexOf("templates/") == 0)) {
+														console.log("Deleting template: " + JSON.stringify(_data));
+              Template.findOneAndRemove(_data, function(err) {
+                if (!err) {
+                  // Dispatch update
+                  console.log("Deleted");
+                  notifyAll();
+                }
+              });
             }
         });
         socket.on('join', function(msg) {
@@ -46,6 +64,15 @@ module.exports = function(server) {
             }
           });
         }
+
+        var notifyAll = function() {
+          Template.find({}, function(err, templates) {
+            if (!err) {
+              console.log("Notify All");
+              templateSocket.emit('message', { method: "UPDATE", uri: "templates", data: templates });
+            }
+          });
+        };
      });
      var adminSocket = socketIO.of('/admin');
      adminSocket.on('connection', function (socket) {
