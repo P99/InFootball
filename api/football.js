256a1c74bb1e670a3bbef68c2a7e207f3443b7fb
Pascal Jacquemart <p99_pascal@yahoo.fr>
2015-09-17 22:41:45 +0100

    Sending questions for specific players - By now a player join latests created game by default

--- api/football.js
+++ api/football.js
@@ -17,12 +17,13 @@ module.exports = function(namespace, io, socket, msg) {
 }
 
 function operatorHandler(io, socket, msg) {
+  var room = socket.rooms.filter(function(key) { return key.indexOf('football-') == 0; });
   switch  (msg.action) {
     case "JOIN":
       gameModel.findById( msg.uri, function( err, doc ) {
         if (!err) {
           // Create the room after the gameId
-          socket.join(msg.uri); 
+          socket.join('football-' + msg.uri); 
           // Reply "MATCH" with the whole match data (teams)
           // Todo inflate the game object with template etc
           socket.emit('football', {action: "MATCH", uri: msg.uri, data: doc});
@@ -31,17 +32,17 @@ function operatorHandler(io, socket, msg) {
       break;
     case "LEAVE":
       // Todo: cleanup the context
-      socket.leave(msg.uri);
+      socket.leave('football-' + msg.uri);
       break;
     case "SELECT":
       // Todo: Grab the question from the database
       // Send to all players
       questionModel.findById( msg.data, function ( err, doc) {
         if (!err) {
-          console.log("Sending question to players: " + JSON.stringify(doc));
+          console.log("Sending question to players in room " + room + ": " + JSON.stringify(doc));
           //io.sockets.in('player').emit("Questions");
           //io.of('player').to('room').emit('football', {uri: msg.uri, data: doc});
-          io.of('player').emit('football', {uri: msg.uri, data: doc});
+          io.of('player').to(room).emit('football', {uri: 'question', data: doc});
         }
       });
       break
@@ -56,12 +57,25 @@ function playerHandler(io, socket, msg) {
       // Retreive ongoing games
       // Todo: Filter rooms named after actual games
       console.log("Known rooms: " + JSON.stringify(io.of('operator').adapter.rooms));
+      var games = [];
+      for (var key in io.of('operator').adapter.rooms) {
+        if (!key.indexOf("football-")) {
+          games.push(key.slice(9));
+        }
+      }
+      console.log("Games IDs: " + JSON.stringify(games));
+      gameModel.find({'_id': { $in: games}}, function(err, docs) {
+        if (!err) {
+          console.log("Games: " + JSON.stringify(docs));
+          socket.emit('football', { uri: 'games', data: docs });
+        }
+      });
       break;
     case "JOIN":
-      socket.join(msg.uri); 
+      socket.join('football-' + msg.uri); 
       break;
     case "LEAVE":
-      socket.leave(msg.uri);
+      socket.leave('football-' + msg.uri);
       break;
     default:
       console.log("Un-handled action: " + msg.action);
--- views/player/party.js
+++ views/player/party.js
@@ -5,7 +5,19 @@ window.addEventListener("load", function (event) {
   var ref = document.getElementById("questions");
   socket.on('football', function(msg) {
     console.log("Received message: " + JSON.stringify(msg));
-    ref.innerHTML += msg.data.caption + "</br>"
+    switch (msg.uri) {
+    case 'games':
+      // Display the list of ongoing games
+      // Allow the player to join a game
+      // Fixme: Joining the first game by default
+      if (msg.data.length) {
+        socket.emit('football', {action: "JOIN", uri: msg.data[0]._id});
+      }
+      break;
+    case 'question':
+      ref.innerHTML += msg.data.caption + "</br>";
+      break;
+    }
   });
   socket.emit('football', {action: "HELLO"});
 
